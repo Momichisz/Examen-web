a13ac361a24b82201d07408a509b05f6bd18639f
Heyder Medina <medinaheyder@gmail.com>
2024-06-30 14:24:14 -0600

    v2

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function start-test() {
-    
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const startButton = document.getElementById('start-button');
+    const startContainer = document.getElementById('start-container');
+    const startImgContainer = document.getElementById('img-container');
+
+    if (startButton) {
+        startButton.addEventListener('click', function () {
+            
+            startContainer.classList.add('hidden');
+            startImgContainer.classList.add('hidden');
+
+            setTimeout(() => {
+                startContainer.style.display = 'none';
+                startImgContainer.style.display = 'none';
+            }, 1000);
+        });
+    }
+});
