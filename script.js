94e3ac38d9829a77ab49a47e4e5ca9ba948d0ff6
Heyder Medina <medinaheyder@gmail.com>
2024-06-30 18:48:15 -0600

    v4  - rules

--- script.js
+++ script.js
@@ -2,17 +2,27 @@ document.addEventListener('DOMContentLoaded', function () {
     const startButton = document.getElementById('start-button');
     const startContainer = document.getElementById('start-container');
     const startImgContainer = document.getElementById('img-container');
+    const testDescContainer = document.getElementById('test-desc');
+    const mainSection = document.getElementById('main-section');
 
     if (startButton) {
         startButton.addEventListener('click', function () {
-            
             startContainer.classList.add('hidden');
             startImgContainer.classList.add('hidden');
 
             setTimeout(() => {
                 startContainer.style.display = 'none';
                 startImgContainer.style.display = 'none';
-            }, 1000);
+
+                testDescContainer.style.display = 'block'; // Make it block before applying opacity
+                requestAnimationFrame(() => { // Ensure the element is rendered before adding class
+                    testDescContainer.classList.add('visible');
+                });
+
+                // Apply centering styles to mainSection
+                mainSection.style.justifyContent = 'center';
+                mainSection.style.alignItems = 'center';
+            }, 500); // Duración de la transición en milisegundos
         });
     }
 });
